35eaad55f97cd5c9e26290e0b147c42e772ddff6
cjporter25 <chris.j.porter25@gmail.com>
2024-04-19 00:22:51 -0500

    Filename bug fixed

--- reporting.py
+++ reporting.py
@@ -11,16 +11,10 @@ class ReportsManager:
         self.primaryDir = folderPath
     def __init__(self):
         self.primaryDir = "C:\\Users\\[USER_PROFILE]\\Desktop\\MPScrubberReports"
-    
     def set_primary_directory(self):
         user_profile = os.environ.get('USERPROFILE')
-        # The following becomes C:\Users\cj_po
+        # user_profile becomes C:\Users\cj_po
         print(user_profile)
-        #if user_profile:
-        #    print("User profile:", user_profile)
-        #else:
-        #    print("User profile not found.")
-
         self.primaryDir = user_profile + "\\Desktop\\MPScrubberReports"
         print(self.primaryDir)
 
@@ -32,23 +26,25 @@ class ReportsManager:
             print("Folder created successfully:", self.primaryDir)
         else:
             print("Folder already exists. No action required.")
+    def build_new_report(self):
+        currDateTime = self.get_current_date_and_time()
+        # Excel file type = .xlsx
+        reportFileName = "MPReport(" + currDateTime + ").xlsx"
+        print(reportFileName)
+        reportFilePath = self.primaryDir + "\\" + reportFileName
+        print(reportFilePath)
+
+        if not os.path.exists(reportFilePath):
+            workbook = Workbook()
+            print(reportFilePath)
+            workbook.save(reportFilePath)
+            print("Excel file created successfully:", reportFilePath)
+        else:
+            pass
     def get_current_date_and_time(self):
         today = date.today()
         currDate = today.strftime("%m-%d-%Y")
         time = datetime.datetime.now().time()
-        currTime = time.strftime("%H:%M:%S")
-        currDateTime = currDate + "." + currTime
+        currTime = time.strftime("%H.%M.%S")
+        currDateTime = currDate + "-" + currTime
         return currDateTime
-    def build_new_report(self):
-        currDateTime = self.get_current_date_and_time()
-        reportName = "MPReport<" + currDateTime + ">"
-        reportFilePath = self.primaryDir + "\\" + reportName
-        try:
-            # Try to open the Excel file (will raise FileNotFoundError if it doesn't exist)
-            with open(reportFilePath):
-                pass  # File exists, do nothing
-        except FileNotFoundError:
-            # File does not exist, create a new Excel file
-            workbook = Workbook()
-            workbook.save(reportFilePath)
-            print("Excel file created successfully:", reportFilePath)
