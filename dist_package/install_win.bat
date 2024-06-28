f637afa54770f4327ad102db840b69393c77a612
cjporter25 <chris.j.porter25@gmail.com>
2024-06-28 17:01:04 -0500

    Cleaned up installation process

--- dist_package/install_win.bat
+++ dist_package/install_win.bat
@@ -19,18 +19,16 @@ pip install -r requirements.txt --verbose
 
 :: List the installed packages (No longer necessary)
 :: pip list
-echo **********************
+echo. 
 echo Installation complete. To run the application, a new terminal window will be opened.
-echo **********************
-echo NOTE: You may be required to allow your computer to run sripts. If so, type in the following
-command within any terminal instance and press enter:
-
+echo.
+echo NOTE: You may be required to allow your computer to run sripts. If so, type in the 
+echo following command within any terminal instance and press enter:
+echo.
 echo ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 echo ' Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser '
 echo ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
+echo.
 :: Open a new terminal window and navigate to the project directory
-start powershell -NoExit -Command "cd '%~dp0'; . .venv\Scripts\Activate.ps1; pip list; echo Virtual environment activated. Type 'python main.py' and press enter to start the application."
-echo ****NOTE: Alternative option in the future will include an application run file to avoid the terminal altogether post installation!
-
+start powershell -NoExit -Command "cd '%~dp0'; . .venv\Scripts\Activate.ps1; { Virtual environment activated... Type 'python main.py' and press [ENTER] to start the application... NOTE: Alternative option in the future will include an application run file to avoid the terminal altogether post installation! }"
 pause
